0d6ab1d657f3547da5d70a904a0a8d8fdd5388b8
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2020-06-10 11:56:15 +0100

    Update ContactPicker.kt

--- android/src/main/kotlin/me/schlaubi/fluttercontactpicker/ContactPicker.kt
+++ android/src/main/kotlin/me/schlaubi/fluttercontactpicker/ContactPicker.kt
@@ -41,7 +41,7 @@ class ContactPicker private constructor(private val pickContext: PickContext, pr
     private fun buildPhoneNumber(cursor: Cursor, activity: Activity): Map<String, String> {
         val phoneType = cursor.getInt(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE))
         val customLabel = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.LABEL))
-        val avatar=cursor.getString(cursor.getColumnIndex(ContactsContract.Photo.CONTENT_DIRECTORY))
+        val avatar=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI))
         print("this is inside plugin")
         val label = ContactsContract.CommonDataKinds.Phone.getTypeLabel(activity.resources, phoneType, customLabel) as String
         val number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
